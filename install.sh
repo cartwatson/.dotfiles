33281ce5b6e70faebb7f9015c51f9803cb72e17d
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-02-20 16:46:39 -0800

    feat: refactor order, add automation based on env

--- install.sh
+++ install.sh
@@ -24,7 +24,6 @@ if [ ! -d ~/work     ]; then mkdir ~/work    ; fi
 
 if ask "Would you like to clone The Index to ~/personal?"; then
     git clone git@github.com:cartwatson/index ~/personal/index
-    # git clone https://github.com/cartwatson/index ~/personal/index
 fi
 
 # hush login
@@ -55,7 +54,7 @@ fi
 
 if ask "Would you like to install vim plugins?"; then
     if [ ! -d ~/.vim ]; then mkdir ~/.vim; fi
-    # clone down the repos for airline + themes
+    # clone down the repos for plugins
     # git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
     # git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/pack/dist/start/vim-airline-themes
     git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
@@ -68,71 +67,72 @@ if ask "Would you like to install vim plugins?"; then
 fi
 
 # dynamically create machine specific aliases
-if ask "Create ~/.bash_machine_aliases.sh?"; then
-    cp ./machine_aliases.sh ~/.bash_machine_aliases.sh
+if [ -f ~/.bash_machine_aliases.sh ]; then cp ~/.bash_machine_alises.sh ~/.bash_machine_aliases.sh.old; fi
+cp ./machine_aliases.sh ~/.bash_machine_aliases.sh
 
-    if ask "Are you using vscode/vscodium?"; then
-        declare -a code_extensions=(
-                                    # "ms-vscode-remote.remote-wsl"  # wsl extension
-                                    "ritwickdey.liveserver"        # live server
-                                    "yzhang.markdown-all-in-one"   # markdown all in one
-                                    "pkief.material-icon-theme"    # material icon theme
-                                    # "swyphcosmo.spellchecker"      # spell checker
-                                    )
+code_extensions=( 
+                "ritwickdey.liveserver"        # live server
+                "yzhang.markdown-all-in-one"   # markdown all in one
+                "pkief.material-icon-theme"    # material icon theme
+                )
 
-        VSCODE="code"
-        if ask "Are you using vscodium?"; then
-            VSCODE="codium"
-        fi
+# windows/wsl instance specific aliases
+if [ -f "/proc/sys/fs/binfmt_misc/WSLInterop" ]; then
+    echo "installing WSL specific files and aliases..."
 
-        ## now loop through the above array
-        for extension in "${code_extensions[@]}"
-        do
-            if ask "Would you like to install $extension?"; then
-                $VSCODE --install-extension $extension
-            fi
-        done
+    echo 'alias e="explorer.exe ." # open windows explorer in current directory' >> ~/.bash_machine_aliases.sh
+    code_extensions+=("ms-vscode-remote.remote-wsl");  # wsl extension
 
-        echo -e 'alias c="$VSCODE ." # open vscode/vscodium in current directory' >> ~/.bash_machine_aliases.sh
-    fi
+    echo -e "WSL files and aliases created\n"
+fi
 
-    # windows/wsl instance specific aliases
-    if ask "Is this a wsl instance?"; then
-        echo 'alias e="explorer.exe ." # open windows explorer in current directory' >> ~/.bash_machine_aliases.sh
-    fi
+if ask "Are you using vscode/vscodium?"; then
+    VSCODE="code"
+    if ask "Are you using vscodium?"; then VSCODE="codium"; fi
 
-    # arch install
-    if ask "Is this an arch machine? (set terminal theme to dark) "; then
-    	ln -s ~/.dotfiles/arch/.Xdefaults ~/.Xdefaults
-    fi
+    # now loop through the above array
+    for extension in "${code_extensions[@]}"
+    do
+        $VSCODE --install-extension $extension
+    done
 
-    if ask "Are you using a window manager? "; then
-        # provide user with a selection of window managers 
-        options=("i3" "Awesome" "Sike, I'm not using a window manager")
-        select opt in "${options[@]}"
-        do
-            case $opt in
-                "i3")
-		            cp ~/.config/i3/config ~/.config/i3/config.old
-                    ln -s ~/.dotfiles/windowManagers/i3/config ~/.config/i3/config
-		            break
-                    ;;
-                "Awesome")
-                    echo "Haven't used awesome yet so nothing to do here"
-		            break
-                    ;;
-                "Sike, I'm not using a window manager")
-                    break
-                    ;;
-                *) echo "invalid option $REPLY";;
-            esac
-        done
+    # create alias
+    echo -e 'alias c="$VSCODE ." # open vscode/vscodium in current directory' >> ~/.bash_machine_aliases.sh
+fi
 
-        # polybar config
-        if ask "Are you using polybar? "; then
-            cp ~/.config/polybar/config.ini ~/.config/polybar/config.ini.old
-            ln -s ~/.dotfiles/windowManagers/misc/polybar/config.ini ~/.config/polybar/config.ini
-        fi
+# arch install
+if [ -f "/etc/arch-release" ]; then
+    echo "installing arch specific files..."
+    ln -s ~/.dotfiles/arch/.Xdefaults ~/.Xdefaults
+    echo -e "arch files installed\n"
+fi
+
+if ask "Are you using a window manager? "; then
+    # provide user with a selection of window managers 
+    options=("i3" "Awesome" "Sike, I'm not using a window manager")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "i3")
+                cp ~/.config/i3/config ~/.config/i3/config.old
+                ln -s ~/.dotfiles/windowManagers/i3/config ~/.config/i3/config
+                break
+                ;;
+            "Awesome")
+                echo "Haven't used awesome yet so nothing to do here"
+                break
+                ;;
+            "Sike, I'm not using a window manager")
+                break
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
+
+    # polybar config
+    if ask "Are you using polybar? "; then
+        cp ~/.config/polybar/config.ini ~/.config/polybar/config.ini.old
+        ln -s ~/.dotfiles/windowManagers/misc/polybar/config.ini ~/.config/polybar/config.ini
     fi
 fi
 
