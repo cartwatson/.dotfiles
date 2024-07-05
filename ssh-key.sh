e98d42f5d64eb0ee9cd394055e61c21437ecc6d5
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-07-05 16:44:11 -0700

    update ssh-keygen script, better filenaming

--- ssh-key.sh
+++ ssh-key.sh
@@ -15,7 +15,15 @@ read -p "What site is the key for? " site
 read -p "What is your email? " email
 read -p "If you would like a passphrase, enter it: " passphrase
 
-ssh-keygen -q -t ed25519 -f ~/.ssh/"$site" -C "$email" -N "$passphrase" || error "generating key"
+# get input for file_name
+read -p "Enter option (w/p): " input
+case "$input" in
+  w) type_of_key="work" ;;
+  p|"") type_of_key="personal" ;;
+esac
+
+file_name="$type_of_key@$site"
+ssh-keygen -q -t ed25519 -f ~/.ssh/"$file_name" -C "$email" -N "$passphrase" || error "generating key"
 
 if [ -n $SSH_AGENT_PID ]; then
   eval "$(ssh-agent -s)" > /dev/null || error "starting ssh-agent"
