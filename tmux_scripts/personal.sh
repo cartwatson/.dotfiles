870b76191ac55729c776d74b8c941c25c8328728
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-09-30 09:23:58 -0700

    rev: update tmux scripts for better setup

--- tmux_scripts/personal.sh
+++ tmux_scripts/personal.sh
@@ -1,14 +1,29 @@
+#!/usr/bin/env bash
+# scipt to open commonly used personal files
+
 SESH="personal"
 
-tmux has-session -t $SESH 2>/dev/null
+function open_tabs() {
+  tmux send-keys -t "$SESH":idx "cds" C-m
+
+  tmux new-window -t "$SESH" -n "dotfiles"
+  tmux send-keys -t "$SESH":dotfiles "cdd" C-m
+
+  tmux new-window -t "$SESH" -n "temp"
+  tmux send-keys -t "$SESH":temp "cdm" C-m
+}
+
+tmux has-session -t "$SESH" 2>/dev/null
 
 if [ $? != 0 ]; then
-  tmux new-session -d -s $SESH -n "idx"
-  # tmux new-window -t $SESH -n "idx"
-  tmux send-keys -t $SESH:idx "cds" C-m
-
-  tmux new-window -t $SESH -n "dotfiles"
-  tmux send-keys -t $SESH:dotfiles "cdd" C-m
+  # session doesn't exist
+  tmux new-session -d -s "$SESH" -n "idx"
+  # tmux new-window -t "$SESH" -n "idx" # session exists, window doesn't
+  open_tabs
 fi
 
-tmux attach-session -t $SESH
+if [ -n "$TMUX" ]; then
+  tmux switch-client -t "$SESH"
+else
+  tmux attach-session -t "$SESH"
+fi
--- tmux_scripts/work.sh
+++ tmux_scripts/work.sh
@@ -1,14 +1,21 @@
+#!/usr/bin/env bash
+# scipt to open commonly used work files
+
 SESH="work"
 
-tmux has-session -t $SESH 2>/dev/null
+tmux has-session -t "$SESH" 2>/dev/null
 
 if [ $? != 0 ]; then
-  tmux new-session -d -s $SESH -n "proj1"
-  # tmux new-window -t $SESH -n "idx"
-  tmux send-keys -t $SESH:proj1 "cdw" C-m
+  tmux new-session -d -s "$SESH" -n "idx-work"
+  # tmux new-window -t "$SESH" -n "idx"
+  tmux send-keys -t "$SESH":idx-work "cd ~/personal/index/work/logs/anduril" C-m
 
-  # tmux new-window -t $SESH -n "dotfiles"
-  # tmux send-keys -t $SESH:dotfiles "cdd" C-m
+  tmux new-window -t "$SESH" -n "proj"
+  tmux send-keys -t "$SESH":proj "cdw" C-m
 fi
 
-tmux attach-session -t $SESH
+if [ -n "$TMUX" ]; then
+  tmux switch-client -t "$SESH"
+else
+  tmux attach-session -t "$SESH"
+fi
