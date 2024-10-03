e11db4951cc05090d2469476db08cec336574138
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-10-02 19:02:46 -0700

    fix: add status + clear on init

--- tmux_scripts/work.sh
+++ tmux_scripts/work.sh
@@ -9,6 +9,7 @@ if [ $? != 0 ]; then
   tmux new-session -d -s "$SESH" -n "idx-work"
   # tmux new-window -t "$SESH" -n "idx"
   tmux send-keys -t "$SESH":idx-work "cd ~/personal/index/work/logs/anduril" C-m
+  tmux send-keys -t "$SESH":idx-work "gsll" C-m
 
   tmux new-window -t "$SESH" -n "proj"
   tmux send-keys -t "$SESH":proj "cdw" C-m
