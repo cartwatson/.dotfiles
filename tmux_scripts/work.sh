688403ff698f8fd8134bf803811751a19e5d1b94
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-10-02 19:21:19 -0700

    fix: simplify tmux switch vs attach

--- tmux_scripts/work.sh
+++ tmux_scripts/work.sh
@@ -8,15 +8,11 @@ tmux has-session -t "$SESH" 2>/dev/null
 if [ $? != 0 ]; then
   tmux new-session -d -s "$SESH" -n "idx-work"
   # tmux new-window -t "$SESH" -n "idx"
-  tmux send-keys -t "$SESH":idx-work "cd ~/personal/index/work/logs/anduril" C-m
-  tmux send-keys -t "$SESH":idx-work "gsll" C-m
+  tmux send-keys -t "$SESH":idx-work "cd ~/personal/index/work/logs/anduril; gsll" C-m
 
   tmux new-window -t "$SESH" -n "proj"
   tmux send-keys -t "$SESH":proj "cdw" C-m
 fi
 
-if [ -n "$TMUX" ]; then
-  tmux switch-client -t "$SESH"
-else
-  tmux attach-session -t "$SESH"
-fi
+# attempt to switch clients, if fails attach
+tmux switch-client -t "$SESH" || tmux attach-session -t "$SESH"
