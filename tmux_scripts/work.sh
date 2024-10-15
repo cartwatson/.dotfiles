2f2948171f9f19c2cb7b52e43eec989fcd14ddf6
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2024-10-15 12:04:47 -0700

    fix: only ll once

--- tmux_scripts/work.sh
+++ tmux_scripts/work.sh
@@ -8,7 +8,7 @@ tmux has-session -t "$SESH" 2>/dev/null
 if [ $? != 0 ]; then
   tmux new-session -d -s "$SESH" -n "idx-work"
   # tmux new-window -t "$SESH" -n "idx"
-  tmux send-keys -t "$SESH":idx-work "cd ~/work/personal-logs; ll" C-m
+  tmux send-keys -t "$SESH":idx-work "cd ~/work/personal-logs" C-m
 
   tmux new-window -t "$SESH" -n "apps"
   tmux send-keys -t "$SESH":apps "cdh" C-m
