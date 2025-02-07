db9716532798bbbd0fd7d575b4c0df4da0108c04
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-02-07 00:34:29 -0800

    fix: formatting

--- nixos/profiles/gnome/default.nix
+++ nixos/profiles/gnome/default.nix
@@ -7,7 +7,7 @@
 
   # Remove gnome default apps
   environment.gnome.excludePackages = (with pkgs; [
-    epiphany              # Web browser
+    # epiphany              # Web browser
     geary                 # Email client
     seahorse              # Password manager
     gnome-calendar
@@ -28,63 +28,62 @@
     profiles.user.databases = [
       {
         lockAll = true; # prevents overriding
-        settings = {
+        settings = lib.fix (self: with lib.gvariant; {
           "org/gnome/desktop/interface" = {
             color-scheme = "prefer-dark";
             gtk-enable-primary-paste = false;
             # show-battery-percentage = true;
           };
 
-          "org/gnome/settings-daemon/plugins" = {
-            # NIGHT SHIFT
-            "color" = {
-              night-light-enabled=true
-              night-light-schedule-automatic=false
-              night-light-schedule-from=19.0
-              night-light-schedule-to=10.0
-            };
-            # KEYBINDINGS
-            "media-keys" = {
-              custom-keybindings = "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/";
-              control-center = ['<Super>i'];
-              screensaver = ['<Super>l'];
-              custom-keybindings/custom0" = {
-                binding = "<Super>e";
-                command = "/usr/bin/env nautilus";
-                name = "File Manager";
-              };
-            };
+          # NIGHT SHIFT
+          "org/gnome/settings-daemon/plugins/color" = {
+            night-light-enabled = true;
+            night-light-schedule-automatic = false;
+            night-light-schedule-from = 19.0;
+            night-light-schedule-to = 10.0;
+          };
+
+          # KEYBINDINGS
+          "org/gnome/settings-daemon/plugins/media-keys" = {
+              custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"];
+              control-center = "<Super>i";
+              screensaver = "<Super>l";
+          };
+
+          "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+            binding = "<Super>e";
+            command = "/usr/bin/env nautilus";
+            name = "File Manager";
           };
           
           "org/gnome/desktop/wm/keybindings" = {
-            move-to-workspace-left=['<Control><Alt><Shift>Left', '<Control><Alt><Shift>h', '<Super><Shift>h'];
-            move-to-workspace-right=['<Control><Alt><Shift>Right', '<Control><Alt><Shift>l', '<Super><Shift>l'];
-            switch-to-workspace-left=['<Control><Alt>Left', '<Control><Alt>h'];
-            switch-to-workspace-right=['<Control><Alt>Right', '<Control><Alt>l'];
-            switch-to-workspace-1=['<Super>1'];
-            switch-to-workspace-2=['<Super>2'];
-            switch-to-workspace-3=['<Super>3'];
-            switch-to-workspace-4=['<Super>4'];
-            switch-to-workspace-5=['<Super>5'];
-            switch-to-workspace-6=['<Super>6'];
-            switch-to-workspace-7=['<Super>7'];
-            switch-to-workspace-8=['<Super>8'];
-            switch-to-workspace-9=['<Super>9'];
+            move-to-workspace-left = ["<Control><Alt><Shift>Left" "<Control><Alt><Shift>h" "<Super><Shift>h"];
+            move-to-workspace-right = ["<Control><Alt><Shift>Right" "<Control><Alt><Shift>l" "<Super><Shift>l"];
+            switch-to-workspace-left = ["<Control><Alt>Left" "<Control><Alt>h"];
+            switch-to-workspace-right = ["<Control><Alt>Right" "<Control><Alt>l"];
+            switch-to-workspace-1 = [ "<Super>1" ];
+            switch-to-workspace-2 = [ "<Super>2" ];
+            switch-to-workspace-3 = [ "<Super>3" ];
+            switch-to-workspace-4 = [ "<Super>4" ];
+            switch-to-workspace-5 = [ "<Super>5" ];
+            switch-to-workspace-6 = [ "<Super>6" ];
+            switch-to-workspace-7 = [ "<Super>7" ];
+            switch-to-workspace-8 = [ "<Super>8" ];
+            switch-to-workspace-9 = [ "<Super>9" ];
           };
 
-          "org/gnome/shell" = {
-            app-switcher.current-workspace-only = true;            
-            "keybindings" = {
-              show-screenshot-ui=['<Shift><Super>s']
-              switch-to-application-1=@as []
-              switch-to-application-2=@as []
-              switch-to-application-3=@as []
-              switch-to-application-4=@as []
-              switch-to-application-5=@as []
-            };
+          "org/gnome/shell/app-switcher".current-workspace-only = true;
+
+          "org/gnome/shell/keybindings" = {
+            show-screenshot-ui = [ "<Shift><Super>s" ];
+            # switch-to-application-1 = [];
+            # switch-to-application-2 = [];
+            # switch-to-application-3 = [];
+            # switch-to-application-4 = [];
+            # switch-to-application-5 = [];
           };
-        };
-      };
+        });
+      }
     ];
   };
 }
--- nixos/profiles/gnome/extensions.nix
+++ nixos/profiles/gnome/extensions.nix
@@ -10,31 +10,31 @@
   ]);
 
   programs.dconf.profiles.user.databases = [{
-    settings = with lib.gvariant; {
+    settings = lib.fix (self: with lib.gvariant; {
       # EXTENSION SPECIFIC SETTINGS
       "org/gnome/shell/extensions/panel-date-format".format = "%Y-%m-%d %H:%M";
 
-      "extensions/just-perfection" = {
+      "org/gnome/shell/extensions/just-perfection" = {
         accessibility-menu=true;
         activities-button=false;
         aggregate-menu=true;
-        animation=1;
+        # animation=1;
         app-menu=false;
         app-menu-icon=true;
         app-menu-label=true;
         background-menu=true;
         clock-menu=true;
-        clock-menu-position=0;
-        clock-menu-position-offset=0;
-        controls-manager-spacing-size=0;
+        # clock-menu-position=0;
+        # clock-menu-position-offset=0;
+        # controls-manager-spacing-size=0;
         dash=true;
         dash-app-running=true;
-        dash-icon-size=0;
+        # dash-icon-size=0;
         dash-separator=true;
         double-super-to-appgrid=true;
         events-button=true;
         keyboard-layout=true;
-        notification-banner-position=1;
+        # notification-banner-position=1;
         osd=true;
         overlay-key=true;
         panel=true;
@@ -43,24 +43,24 @@
         ripple-box=true;
         search=true;
         show-apps-button=true;
-        startup-status=1;
+        # startup-status=1;
         switcher-popup-delay=true;
         theme=false;
-        top-panel-position=0;
+        # top-panel-position=0;
         weather=true;
         window-demands-attention-focus=false;
         window-picker-icon=true;
         window-preview-caption=true;
         window-preview-close-button=true;
         workspace=true;
-        workspace-background-corner-size=0;
+        # workspace-background-corner-size=0;
         workspace-peek=true;
         workspace-popup=true;
-        workspace-switcher-size=0;
+        # workspace-switcher-size=0;
         workspace-wrap-around=false;
         workspaces-in-app-grid=true;
         world-clock=true;
       };
-    };
+    });
   }];
 }
