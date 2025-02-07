466085d074148b78318176c4d8ba2d8dd9d32ae2
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-02-06 23:32:48 -0800

    feat: gnome in nix!

---
+++ nixos/profiles/gnome/extensions.nix
@@ -0,0 +1,66 @@
+{ config, pkgs, lib, self, ... }:
+
+{
+  environment.systemPackages = (with pkgs; [
+    gnome-tweaks
+  ]) ++ (with pkgs.gnomeExtensions; [
+    blur-my-shell
+    just-perfection
+    panel-date-format
+  ]);
+
+  programs.dconf.profiles.user.databases = [{
+    settings = with lib.gvariant; {
+      # EXTENSION SPECIFIC SETTINGS
+      "org/gnome/shell/extensions/panel-date-format".format = "%Y-%m-%d %H:%M";
+
+      "extensions/just-perfection" = {
+        accessibility-menu=true;
+        activities-button=false;
+        aggregate-menu=true;
+        animation=1;
+        app-menu=false;
+        app-menu-icon=true;
+        app-menu-label=true;
+        background-menu=true;
+        clock-menu=true;
+        clock-menu-position=0;
+        clock-menu-position-offset=0;
+        controls-manager-spacing-size=0;
+        dash=true;
+        dash-app-running=true;
+        dash-icon-size=0;
+        dash-separator=true;
+        double-super-to-appgrid=true;
+        events-button=true;
+        keyboard-layout=true;
+        notification-banner-position=1;
+        osd=true;
+        overlay-key=true;
+        panel=true;
+        panel-in-overview=true;
+        power-icon=true;
+        ripple-box=true;
+        search=true;
+        show-apps-button=true;
+        startup-status=1;
+        switcher-popup-delay=true;
+        theme=false;
+        top-panel-position=0;
+        weather=true;
+        window-demands-attention-focus=false;
+        window-picker-icon=true;
+        window-preview-caption=true;
+        window-preview-close-button=true;
+        workspace=true;
+        workspace-background-corner-size=0;
+        workspace-peek=true;
+        workspace-popup=true;
+        workspace-switcher-size=0;
+        workspace-wrap-around=false;
+        workspaces-in-app-grid=true;
+        world-clock=true;
+      };
+    };
+  }];
+}
