027585f3691dc2babdc553e5865f16d334ec4685
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-02-19 14:56:18 -0800

    minor changes

--- nixos/hosts/jupiter/default.nix
+++ nixos/hosts/jupiter/default.nix
@@ -66,11 +66,7 @@
 
 # PACKAGES --------------------------------------------------------------------
   # Allow unfree packages
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
-  };
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
