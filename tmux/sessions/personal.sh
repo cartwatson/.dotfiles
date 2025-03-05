c6da1eeed44fc4081cfbbbdde03f13a03eec6c44
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-03-05 15:31:54 -0800

    fix: more extensible check to see if on home machine for personal tmux session launcher

--- tmux/sessions/personal.sh
+++ tmux/sessions/personal.sh
@@ -14,7 +14,8 @@ function open_tabs() {
   tmux new-window -t "$SESH" -n "dotfiles"
   tmux send-keys -t "$SESH":dotfiles "cdd" C-m
 
-  if [[ ! $(hostname) =~ "watson" ]]; then
+  # NOTE: this assumes that the machine this is running on is a nix machine but lucky for me I only run nix machines :)
+  if [[ -d ~/.dotfiles/nixos/hosts/$(HOSTNAME) ]]; then
     tmux new-window -t "$SESH" -n "proxmox"
     tmux send-keys -t "$SESH":proxmox "cd ~/personal/proxmox; gsll" C-m
   fi
