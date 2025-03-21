3c046e6b02555bfb46066c07b86b12f85a388689
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-03-21 10:15:52 -0700

    chore: consolidate gnome information, remove comments from defaults

--- nixos/hosts/orion/default.nix
+++ nixos/hosts/orion/default.nix
@@ -1,5 +1,3 @@
-# Help is available in the configuration.nix(5) man page and in the NixOS manual (Run ‘nixos-help’).
-
 { config, pkgs, unstable, inputs, ... }:
 
 {
@@ -19,27 +17,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -62,25 +41,6 @@
 
   # Enable touchpad support (enabled by default in most desktopManager).
   services.libinput.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- nixos/profiles/gnome/default.nix
+++ nixos/profiles/gnome/default.nix
@@ -5,6 +5,19 @@
     ./extensions.nix
   ];
 
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
   # Remove gnome default apps
   environment.gnome.excludePackages = (with pkgs; [
     epiphany              # Web browser
