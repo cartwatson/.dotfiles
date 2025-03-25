dad69638469cad4f74be009d6eb80bed9c44fe6e
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-03-24 20:51:50 -0700

    fix: sunshine config

--- nixos/modules/gaming/sunshine.nix
+++ nixos/modules/gaming/sunshine.nix
@@ -10,11 +10,20 @@
   ]);
 
   services.sunshine = {
-    enable = true;
+    # start service by running `sunshine` in terminal
+    enable = false;
+    autoStart = false;
+    capSysAdmin = true;
     openFirewall = true;
-    applications.apps = [
-      steam
-      prismlauncher
+  };
+
+  networking.firewall = {
+    enable = true;
+    # mystery process on 48010, disabled
+    allowedTCPPorts = [ 47984 47989 47990 ];
+    allowedUDPPortRanges = [
+      { from = 47998; to = 48000; }
+      { from = 8000; to = 8010; }
     ];
   };
 }
