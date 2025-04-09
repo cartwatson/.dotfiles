a84dc2dadca535e6db748686fcd236714494d378
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-04-09 14:16:45 -0700

    add: more lang servers! (how did I not have nil bro)

--- nixos/modules/helix.nix
+++ nixos/modules/helix.nix
@@ -4,8 +4,11 @@
   environment.systemPackages = (with pkgs; [
     helix
     # LSPs for helix
-    marksman
-    bash-language-server
-    python312Packages.python-lsp-server
+    nil                                     # nix
+    marksman                                # markdown
+    bash-language-server                    # bash... duh
+    python312Packages.python-lsp-server     # python
+    rust-analyzer                           # rust
+    gopls                                   # go
   ]);
 }
