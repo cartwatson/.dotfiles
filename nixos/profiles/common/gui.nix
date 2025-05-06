de1a0b4fc0613751ecd1f46223e2e5bcb697cedc
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-05-06 16:23:49 -0700

    fix: keep main apps updated; feat: add bitwarden extension to chromium

--- nixos/profiles/common/gui.nix
+++ nixos/profiles/common/gui.nix
@@ -1,12 +1,12 @@
 # IMPORTANT: Included pkgs & settings for **ALL GUI** configurations
 
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, pkgs-unstable, ... }:
 
 {
   imports = [
   ];
 
-  environment.systemPackages = (with pkgs; [
+  environment.systemPackages = (with pkgs-unstable; [
     vscodium
 
     chromium
@@ -17,4 +17,8 @@
 
     drawing
   ]);
+
+  programs.chromium.extensions = [
+    "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+  ]
 }
