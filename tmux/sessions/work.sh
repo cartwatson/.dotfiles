85c388f9d23e1d747534d21c1fe57e16fd92b9bd
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-07-10 09:58:40 -0700

    fix: tmux: new script paths + new idx layout

--- tmux/sessions/work.sh
+++ tmux/sessions/work.sh
@@ -8,13 +8,9 @@ tmux has-session -t "$SESH" 2>/dev/null
 if [ $? != 0 ]; then
   window="idx"
   tmux new-session   -d -s "$SESH" -n "$window"
-  tmux split-window  -t "$SESH":"$window".1 -h
-  tmux split-window  -t "$SESH":"$window".1 -h
-  tmux send-keys     -t "$SESH":"$window".1 "cd ~/work/idx; gsll" C-m
-  tmux send-keys     -t "$SESH":"$window".2 "cd ~/work/idx; hx daily-log.md notes/personal-meeting-notes.md" C-m
-  tmux send-keys     -t "$SESH":"$window".3 "cd ~/work/idx/notes; hx useful_commands.md reference.md" C-m
-  tmux select-layout -t "$SESH":"$window" even-horizontal
-  tmux select-pane   -t "$SESH":"$window".1
+  tmux split-window  -t "$SESH:$window".1 -h
+  tmux send-keys     -t "$SESH:$window".1 "cd ~/work/idx; hx daily-log.md notes/personal-meeting-notes.md" C-m
+  tmux send-keys     -t "$SESH:$window".2 "cd ~/work/idx/notes; hx useful_commands.md reference.md" C-m
 
   window="btop"
   tmux new-window    -t "$SESH" -n "$window"
@@ -23,19 +19,21 @@ if [ $? != 0 ]; then
 
   window="apps"
   tmux new-window    -t "$SESH" -n "$window"
-  tmux send-keys     -t "$SESH":"$window" "cdw; sudo ./apt+snap-update-upgrade.sh; ../sources/update_sources.sh" C-m
+  tmux send-keys     -t "$SESH:$window" "cdw; sudo ./apt+snap-update-upgrade.sh; ../sources/update_sources.sh" C-m
 
   window="croc-hc"
   tmux new-window    -t "$SESH" -n "$window"
-  tmux send-keys     -t "$SESH":"$window" "cdw; ./health-checks/croc.sh" C-m
+  tmux send-keys     -t "$SESH:$window" "cd $HOME/work/scripts/health-checks; ./crocodile-health-check.sh" C-m
 
   window="talos-hc"
   tmux new-window    -t "$SESH" -n "$window"
-  tmux send-keys     -t "$SESH":"$window" "cdw; ./health-checks/talos.sh" C-m
+  tmux send-keys     -t "$SESH:$window" "cd $HOME/work/scripts/health-checks; ./talos-health-check.sh" C-m
 
   # new window
   tmux new-window    -t "$SESH"
-  tmux send-keys     -t "$SESH":3 "cdw" C-m
+
+  # select first window
+  tmux select-pane   -t "$SESH:idx".1
 fi
 
 # attempt to switch clients, if fails attach
