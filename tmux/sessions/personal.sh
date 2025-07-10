0b9512ba317bd14355fe24d6cdd7ee3eeed8534e
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-07-10 09:56:35 -0700

    fix: tmux: spacing for readability

--- tmux/sessions/personal.sh
+++ tmux/sessions/personal.sh
@@ -7,28 +7,30 @@ tmux has-session -t "$SESH" 2>/dev/null
 
 if [ $? != 0 ]; then
   window="idx"
-  tmux new-session -d -s "$SESH" -n "$window"
-  tmux split-window -t "$SESH":"$window".1 -h
-  tmux send-keys -t "$SESH":"$window".1 "cds" C-m
-  tmux send-keys -t "$SESH":"$window".2 "cds; hx" C-m
-  tmux select-pane -t "$SESH":"$window".1
+  tmux new-session   -d -s "$SESH" -n "$window"
+  tmux split-window  -t "$SESH:$window".1 -h
+  tmux send-keys     -t "$SESH:$window".1 "cds" C-m
+  tmux send-keys     -t "$SESH:$window".2 "cds; hx" C-m
+  tmux select-pane   -t "$SESH:$window".1
 
-  tmux new-window -t "$SESH" -n "dotfiles"
-  tmux send-keys -t "$SESH":dotfiles.1 "cd $HOME/.dotfiles" C-m
-  tmux split-window -t "$SESH":dotfiles.1 -h -c "$HOME/.dotfiles"
-  tmux split-window -t "$SESH":dotfiles.1 -v -c "$HOME/.dotfiles"
-  tmux send-keys -t "$SESH":dotfiles.2
-  tmux send-keys -t "$SESH":dotfiles.3 "hx ." C-m
-  tmux select-pane -t "$SESH":"dotfiles".3
+  window="dotfiles"
+  tmux new-window    -t "$SESH" -n "$window"
+  tmux send-keys     -t "$SESH:$window".1 "cd $HOME/.dotfiles" C-m
+  tmux split-window  -t "$SESH:$window".1 -h -c "$HOME/.dotfiles"
+  tmux split-window  -t "$SESH:$window".1 -v -c "$HOME/.dotfiles"
+  tmux send-keys     -t "$SESH:$window".2
+  tmux send-keys     -t "$SESH:$window".3 "hx ." C-m
+  tmux select-pane   -t "$SESH:$window".3
 
   # NOTE: this assumes that the machine this is running on is a nix machine but lucky for me I only run nix machines :)
   if [[ -d ~/.dotfiles/nixos/hosts/$HOSTNAME ]]; then
-    tmux new-window -t "$SESH" -n "proxmox"
-    tmux send-keys -t "$SESH":proxmox "cd ~/personal/proxmox; gsll" C-m
+    window="proxmox"
+    tmux new-window  -t "$SESH" -n "$window"
+    tmux send-keys   -t "$SESH:$window" "cd ~/personal/proxmox; gsll" C-m
   fi
 
-  tmux new-window -t "$SESH" -n "temp"
-  tmux send-keys -t "$SESH":temp "cdm" C-m
+  # new window
+  tmux new-window -t "$SESH"
 fi
 
 # attempt to switch clients, if fails attach
