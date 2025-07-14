70538fb7ffd4f53a333168bf5254b86a943bafa1
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-07-14 11:59:26 -0700

    fix: tmux: minor updates to ide

--- tmux/sessions/windows/ide.sh
+++ tmux/sessions/windows/ide.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # scipt to open IDE like env in existing tmux from current or specified folder
+# INSTRUCTIONS: navigate to dir to open ide in, run ide, profit
 
 function open_ide() {
-  file_path=$1
-  cd $file_path > /dev/null
+  file_path="$1"
+
+  curr_window_num=$(tmux display-message -p -F '#{window_index}')
+  echo "curr_window_num: $curr_window_num"
 
   # get curr/provided dir for window name (remove '.' for ease)
   window=$(pwd | awk -F / '{print $NF}' | sed 's/\.//') || window="IDE"
@@ -15,22 +18,24 @@ function open_ide() {
     exit
   fi
 
+  # create new window + panes
   tmux new-window -n "$window"
-
   tmux split-window -t "$SESH":"$window".1 -h
   tmux split-window -t "$SESH":"$window".1 -v
 
-  # auto open nix develop if available
+  # init panes with default commands
+  # auto open `nix develop` if available
   if [ -f ./shell.nix ]; then
     tmux send-keys -t "$SESH":"$window".1 "nix develop" C-m
   fi
-
   tmux send-keys -t "$SESH":"$window".2 "clear; gs" C-m
   tmux send-keys -t "$SESH":"$window".3 "hx ." C-m
 
+  # configure + move panes & windows
+  tmux resize-pane -t "$SESH":"$window".3 -x 65%
   tmux select-pane -t "$SESH":"$window".3
-
-  cd - > /dev/null
+  tmux kill-window -t "$SESH:$curr_window_num"
+  tmux move-window -t "$SESH" -r
 }
 
 if [ -z $TMUX ]; then
