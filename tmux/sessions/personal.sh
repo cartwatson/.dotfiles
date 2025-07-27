61503820b52c681ee7721b7a158e1dcd3c66e9ce
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-07-27 15:44:05 -0700

    fix: tmux: personal session launcher

--- tmux/sessions/personal.sh
+++ tmux/sessions/personal.sh
@@ -7,9 +7,15 @@ tmux has-session -t "$SESH" 2>/dev/null
 
 if [ $? != 0 ]; then
   window="idx"
-  tmux new-session   -d -s "$SESH" -n "$window" -c "$HOME/personal/index"
-  tmux split-window  -t "$SESH:$window".1 -h    -c "$HOME/personal/index" "hx ." C-m
-  tmux select-pane   -t "$SESH:$window".1
+  tmux new-session   -d -s "$SESH" -n "$window" -c "$HOME/personal/idx"
+  tmux split-window  -t "$SESH:$window".1 -h    -c "$HOME/personal/idx"
+  tmux send-keys     -t "$SESH:$window".1 "hx TODO.md" C-m
+  tmux send-keys     -t "$SESH:$window".2 "hx ." C-m
+
+  window="btop"
+  tmux new-window    -t "$SESH" -n "$window"
+  tmux move-window   -s "$SESH:$window" -t "$SESH":0
+  tmux send-keys     -t "$SESH:$window" "btop" C-m
 
   window="dotfiles"
   tmux new-window    -t "$SESH" -n "$window" -c "$HOME/.dotfiles"
