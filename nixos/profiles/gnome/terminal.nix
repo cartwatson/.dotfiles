44530a3c1aa9fa611443aa7d68359da0eea5a7f5
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-08-05 15:34:24 -0700

    fix: nixos: gnome terminal background color gruvbox hard contrast

--- nixos/profiles/gnome/terminal.nix
+++ nixos/profiles/gnome/terminal.nix
@@ -41,7 +41,7 @@
             cursor-colors-set=false;
             foreground-color="rgb(235,219,178)";
             highlight-colors-set=false;
-            palette=["rgb(29,32,33)" "rgb(204,36,29)" "rgb(152,151,26)" "rgb(215,153,33)" "rgb(69,133,136)" "rgb(177,98,134)" "rgb(104,157,106)" "rgb(168,153,132)" "rgb(40,40,40)" "rgb(251,73,52)" "rgb(184,187,38)" "rgb(250,189,47)" "rgb(131,165,152)" "rgb(211,134,155)" "rgb(142,192,124)" "rgb(235,219,178)"];
+            palette=["rgb(28,28,28)" "rgb(204,36,29)" "rgb(152,151,26)" "rgb(215,153,33)" "rgb(69,133,136)" "rgb(177,98,134)" "rgb(104,157,106)" "rgb(168,153,132)" "rgb(40,40,40)" "rgb(251,73,52)" "rgb(184,187,38)" "rgb(250,189,47)" "rgb(131,165,152)" "rgb(211,134,155)" "rgb(142,192,124)" "rgb(235,219,178)"];
             use-theme-colors=false;
             use-theme-transparency=true;
             use-transparent-background=false;
