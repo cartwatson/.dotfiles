b6fc9b995af5a2b173e10b32645cc6d9d4418b6b
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-08-27 12:49:34 -0700

    rm: option for extensions, unnecessary and too complex

--- nixos/profiles/gnome/default.nix
+++ nixos/profiles/gnome/default.nix
@@ -3,19 +3,14 @@
 {
   options.custom.services.gnome = {
     enable = lib.mkEnableOption "Setup Gnome";
-    extensions = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-    };
   };
 
-  imports = lib.optional config.custom.services.gnome.enable [
+  imports = [
     ./terminal.nix
-    (lib.mkIf config.custom.services.gnome.extensions ./extensions.nix)
+    ./extensions.nix
   ];
 
   config = lib.mkIf config.custom.services.gnome.enable {
-
     # Enable the GNOME Desktop Environment.
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
