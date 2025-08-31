5262ac6425b13fa307f66e1239bf528168e633e3
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-08-31 13:58:01 -0700

    fix: nix: helix + lsps

--- nixos/modules/helix.nix
+++ nixos/modules/helix.nix
@@ -3,28 +3,23 @@
 {
   options.custom.services.helix = {
     enable = lib.mkEnableOption "Enable Helix and LSPs.";
-    LSPs = lib.mkOption {
-      type = lib.types.listOf lib.types.package;
-      default = (with pkgs; [
-        # languages
-        bash-language-server                    # bash... duh
-        python312Packages.python-lsp-server     # python
-        rust-analyzer                           # rust
-        gopls                                   # go
-        # configs
-        nil                                     # nix
-        yaml-language-server                    # yaml
-        vscode-langservers-extracted            # html, css, json
-        # misc
-        marksman                                # markdown
-      ]);
-      description = "Additional LSP servers.";
-    };
   };
 
   config = lib.mkIf config.custom.services.helix.enable {
     environment.systemPackages = (with pkgs-unstable; [
       helix
+    ]) ++ (with pkgs; [
+      # languages
+      bash-language-server                    # bash... duh
+      python312Packages.python-lsp-server     # python
+      rust-analyzer                           # rust
+      gopls                                   # go
+      # configs
+      nil                                     # nix
+      yaml-language-server                    # yaml
+      vscode-langservers-extracted            # html, css, json
+      # misc
+      marksman                                # markdown
     ]);
   };
 }
