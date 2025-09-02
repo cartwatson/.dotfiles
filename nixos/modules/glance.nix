9edcf8f78fb3d65833b7e05aa20a0cf91c73746a
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-09-01 17:47:50 -0700

    minor changes

--- nixos/modules/glance.nix
+++ nixos/modules/glance.nix
@@ -1,22 +1,19 @@
 { config, lib, pkgs, pkgs-unstable, ... }:
 
 {
-  imports = [
-  ];
-
   options.custom.services.glance = {
     enable = lib.mkEnableOption "Enable Glance.";
     port = lib.mkOption {
       type = lib.types.port;
       default = 8001;
-      description = "Port for the SSH server and related services/protections.";
+      description = "Port for the glance server.";
     };
   };
 
   config = lib.mkIf config.custom.services.glance.enable {
     services.glance = {
       enable = true;
-      openFirewall = true; # DEBUG
+      openFirewall = true; # DEBUG: remove after caddy
       settings = {
         server.port = config.custom.services.glance.port;
         # pages = {};
