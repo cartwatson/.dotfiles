e4c8be63af0d700e85c1f29fc9fed6f53a049bee
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-12-06 22:18:06 -0800

    update: 25.05 -> 25.11

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-bleedingedge.url = "github:nixos/nixpkgs/master";
 
