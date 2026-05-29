0b9bf86e8c4406deab1c018fca0899174d81946e
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2026-05-29 07:03:23 +0000

    fix: glance setup

--- nixos/modules/services/glance/default.nix
+++ nixos/modules/services/glance/default.nix
@@ -3,6 +3,33 @@
 let
   baseCfg = config.custom.services;
   cfg = baseCfg.glance;
+
+  configFile = pkgs.writeText "/var/lib/secrets/glance/env"
+    ''
+      # Theme - HSL Values
+      ## Gruvbox - Dark mode - https://github.com/morhetz/gruvbox?tab=readme-ov-file#palette
+      ### MISC
+      GRUVBOX_BG="195 6 12"
+      GRUVBOX_FG="48 86 88"
+
+      ### upper values in color chart
+      GRUVBOX_RED_DARK="2 75 46"
+      GRUVBOX_GREEN_DARK="60 71 35"
+      GRUVBOX_YELLOW_DARK="40 73 49"
+      GRUVBOX_BLUE_DARK="183 33 40"
+      GRUVBOX_PURPLE_DARK="333 34 54"
+      GRUVBOX_AQUA_DARK="122 21 51"
+      GRUVBOX_ORANGE_DARK="24 88 45"
+
+      ### bottom values in color chart -- 9-15 + orange
+      GRUVBOX_RED="9 96 59"
+      GRUVBOX_GREEN="61 66 44"
+      GRUVBOX_YELLOW="61 66 44"
+      GRUVBOX_BLUE="157 16 58"
+      GRUVBOX_PURPLE="344 47 68"
+      GRUVBOX_AQUA="104 35 62"
+      GRUVBOX_ORANGE="27 96 54"
+    '';
 in
 {
   options.custom.services.glance = {
@@ -37,7 +64,7 @@ in
       enable = true;
       package = pkgs-unstable.glance;
       openFirewall = false;
-      # environmentFile = "/var/lib/secrets/glance/env";
+      environmentFile = configFile;
       settings = {
         theme = {
           background-color    = "\${GRUVBOX_BG}";
@@ -57,7 +84,6 @@ in
         };
         pages = [
           (import ./home.nix)
-          (import ./nova.nix)
         ];
       };
     };
