11323929f5f06ffdec9ccc21dc95a43608fa31f1
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2026-08-01 00:34:35 -0700

    init: ctf module for DEFCON34

---
+++ nixos/modules/services/ctf.nix
@@ -0,0 +1,59 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.custom.services.ctf;
+in
+{
+  options.custom.services.ctf = {
+    enable = lib.mkEnableOption "CTF tools for reverse engineering, pwn, crypto, and forensics";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      # Reverse engineering
+      ghidra
+      cutter
+      rizin
+
+      # Binary exploitation
+      gdb
+      gdbserver
+      checksec
+      patchelf
+      ropper
+      one_gadget
+
+      # Symbolic execution / constraint solving
+      z3
+
+      # Cryptography
+      sage
+      hashcat
+      john
+
+      # Forensics
+      binwalk
+      sleuthkit
+      foremost
+      exiftool
+      yara
+
+      # Web
+      burpsuite
+      mitmproxy
+
+      # Mobile
+      apktool
+      jadx
+
+      # Firmware / emulation
+      qemu
+      upx
+
+      # Miscellaneous
+      socat
+      volatility3
+      radamsa
+    ];
+  };
+}
