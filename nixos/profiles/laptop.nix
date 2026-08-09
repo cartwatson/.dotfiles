0945f2197976e7374140a1fc206d55222b532cfc
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2026-08-08 22:10:25 -0700

    chore: better org

--- nixos/profiles/laptop.nix
+++ nixos/profiles/laptop.nix
@@ -8,13 +8,20 @@ in
     enable = lib.mkEnableOption "Enable default laptop config.";
   };
 
-  config = lib.mkIf (cfg.enable && config.pillar.profiles.desktop.desktopEnvironment == "gnome") {
-    pillar.services.gnome.numWorkspaces = 3;
-    pillar.services.gnome.extensions.automoveWindows = [
-      "org.gnome.Terminal.desktop:1"
-      "firefox.desktop:2"
-      "chromium-browser.desktop:2"
-      "org.gnome.Settings.desktop:3"
-    ];
-  };
+  config = lib.mkMerge [
+    (lib.mkIf (cfg.enable && config.pillar.profiles.desktop.desktopEnvironment == "gnome") {
+      pillar.services.gnome.numWorkspaces = 3;
+      pillar.services.gnome.extensions.automoveWindows = [
+        "org.gnome.Terminal.desktop:1"
+        "firefox.desktop:2"
+        "chromium-browser.desktop:2"
+        "org.gnome.Settings.desktop:3"
+      ];
+    })
+    (lib.mkIf (cfg.enable) {
+      environment.systemPackages = (with pkgs; [
+        # TODO: something here later I guess, good infra to have
+      ]);
+    })
+  ];
 }
